bcfd80ad60b9ce53b151e2372b7e0f5581f70fc1
Benjamin Gonzalez <benjamin.gonzalez.h@usach.cl>
2023-11-01 20:38:06 -0300

    Microservicios Básico (Inicialización)

---
+++ backend-archivos-service/src/main/java/TopEducation/m1/BackendEstudiantesServiceApplication.java
@@ -0,0 +1,13 @@
+package TopEducation.m1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEstudiantesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendEstudiantesServiceApplication.class, args);
+	}
+
+}
